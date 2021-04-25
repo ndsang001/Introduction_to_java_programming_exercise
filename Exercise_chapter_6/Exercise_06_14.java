737eea5370cd8ef1df4eb2fbeed92d7217fc6e2f
ndsang001 <sangdinh2001@gmail.com>
2021-04-25 17:10:55 +0300

    Add the comment

--- Exercise_chapter_6/Exercise_06_14.java
+++ Exercise_chapter_6/Exercise_06_14.java
@@ -14,36 +14,35 @@ import java.util.Scanner;
  */
 public class Exercise_06_14 {
     public static void main(String[] args){
+        //Declare the object and initialize 
+        //with predefined standard input object
         Scanner input =  new Scanner(System.in);
-        
+        //Get input from user
         System.out.print("Enter a integer number: ");
         int number = input.nextInt();
-        
+        //Print the output
         System.out.println("The value of pi with i = " + number + " is " + pi(number));
-        
-        /*System.out.println("i      m(i)");
-            for (int i = 1; i <= 901; i += 100) {
-            System.out.printf("%-3d    %-5.4f\n", i, pi(i));
-         }*/
         
         System.out.println("\ni           m(i)     ");
 	    System.out.println("---------------------");
-	    for (int i = 1; i <= 901; i += 100) {
+	    //Iterative loop for printing pi value
+        for (int i = 1; i <= 901; i += 100) {
             System.out.printf("%-12d", i);
             System.out.printf("%-6.4f\n", pi(i));
 	    }
+        //Close the input object to prevent data leak
+        input.close();
     }
-    
+    //pi method for calculating pi value
     public static double pi(int i){
         double x = 0;
         double piValue = 4;
-        
+        //Iterative loop for 
         for(int j = 1; j <= i; ++j){
-
+            //Finding pi value formula
             x += (pow(-1,j+1)/(2*j-1));
         
         }
-     
         return piValue*x;
     }
 }
